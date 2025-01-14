33ad4f46f942fb1c3aba4186de9e297ddea78e1f
MahimAja <128049499+MahimAja@users.noreply.github.com>
2025-01-14 19:14:35 +0600

    Update hydra_panel.sh

--- hydra_panel.sh
+++ hydra_panel.sh
@@ -35,8 +35,30 @@ npm run seed
 echo -e "\e[35m💜 Creating a new user...\e[0m"
 npm run createUser
 
+# Install Ngrok
+echo -e "\e[35m💜 Installing Ngrok...\e[0m"
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+    | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+    && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+    | sudo tee /etc/apt/sources.list.d/ngrok.list \
+    && sudo apt update \
+    && sudo apt install -y ngrok
+
+# Configure Ngrok with an auth token
+echo -e "\e[35m💜 Please enter your Ngrok auth token:\e[0m"
+read -p "Auth Token: " the_auth_token
+ngrok config add-authtoken $the_auth_token
+
+# Display Ngrok setup success
+echo -e "\e[35m💜 Ngrok setup complete! You can use Ngrok to expose your local server.\e[0m"
+
 # Start the application
 echo -e "\e[35m💜 Starting the Oversee application...\e[0m"
 node .
+
+# Provide instructions for using Ngrok
+echo -e "\e[35m💜 To start Ngrok, use the following command:\e[0m"
+echo -e "\e[33mngrok http 3000\e[0m"
+echo -e "\e[35m💜 Replace '3000' with the port your application is running on if different.\e[0m"
 
 echo -e "\e[35m💜 Setup complete! Enjoy using Oversee. 💜\e[0m"
